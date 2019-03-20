dff789a6764dcaf039d11efd4ce8095f4542fecd
seb <seb@woowahan.com>
2019-03-20 22:20:41 +0900

    woring, resting time에 따른 액션처리

--- pocketdoro/src/extension.ts
+++ pocketdoro/src/extension.ts
@@ -2,11 +2,12 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-let myStatusBarItem: vscode.StatusBarItem;
+let workingTimeBar: vscode.StatusBarItem;
+let restingTimeBar: vscode.StatusBarItem;
 
-let status: string;
 const REST_STATUS = 'REST_STATUS';
 const WORK_STATUS = 'WORK_STATUS';
+let status: string = WORK_STATUS;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,48 +15,65 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
   // register a command that is invoked when the status bar
   // item is selected
   const myCommandId = 'extension.digidoro';
-  subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {}));
+  subscriptions.push(vscode.commands.registerCommand(myCommandId, () => { }));
 
   // create a new status bar item that we can now manage
-  myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-  myStatusBarItem.command = myCommandId;
-  myStatusBarItem.show();
-  subscriptions.push(myStatusBarItem);
+  workingTimeBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  workingTimeBar.command = myCommandId;
+  workingTimeBar.show();
+  subscriptions.push(workingTimeBar);
+
+  restingTimeBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  restingTimeBar.command = myCommandId;
+  restingTimeBar.show();
+  subscriptions.push(restingTimeBar);
 
   // update status bar item once at start
-  updateStatusBarItem(10);
+  updateStatusBarItem(10, 10);
 }
 
-function updateStatusBarItem(restTime: Number): void {
-  let n = 0;
+function updateStatusBarItem(workPeriod: Number, restPeriod: Number): void {
+  let workingTime = 0;
+  let restingTime = 0;
   setInterval(() => {
     if (status === REST_STATUS) {
       return;
     }
-    n++;
-    myStatusBarItem.text = `$(megaphone) working ${n} time`;
-    if (restTime === n) {
-      n = 0;
+    workingTime++;
+    workingTimeBar.text = `$(megaphone) working ${workingTime} time`;
+    if (workPeriod === workingTime) {
+      workingTime = 0;
       status = REST_STATUS;
-      myStatusBarItem.text = `$(megaphone) working ${n} time`;
+      workingTimeBar.hide();
+      restingTimeBar.show();
       rest();
+    }
+  }, 1000);
+
+  setInterval(() => {
+    if (status === WORK_STATUS) {
+      return;
+    }
+    restingTime++;
+    restingTimeBar.text = `$(megaphone) resting ${restingTime} time`;
+    if (restingTime === restPeriod) {
+      restingTime = 0;
+      status = WORK_STATUS;
+      workingTimeBar.show();
+      restingTimeBar.hide();
     }
   }, 1000);
 }
 
-function getNumberOfSelectedLines(editor: vscode.TextEditor | undefined): number {
-  let lines = 0;
-  if (editor) {
-    lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0);
-  }
-  return lines;
-}
-
 function rest() {
+  // console.log(status);
+  if (status === WORK_STATUS) {
+    return;
+  }
   vscode.window.showInformationMessage('Hello World!', { modal: true }).then(() => {
     rest();
   });
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
